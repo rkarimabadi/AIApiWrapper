d34c6edd46fe2ef276a2f2a922796ef36461e94c
rkarimabadi <rkarimabadi@gmail.com>
2024-10-02 15:30:02 +0000

    Deploying to gh-pages from @ rkarimabadi/AIApiWrapper@0900e126a34171ddf8fa10c2bf9190558ac80ecf 🚀

--- service-worker-assets.js
+++ service-worker-assets.js
@@ -418,7 +418,7 @@
     },
     {
       "hash": "sha256-0Dpj\/NkVD5WqSC16ZvJLmlc38iv9+Je+umYBVOycPVU=",
-      "url": "_framework\/dotnet.runtime.8.0.8.mbdpnrzzmt.js"
+      "url": "_framework\/dotnet.runtime.8.0.8.gjn324r0ni.js"
     },
     {
       "hash": "sha256-SZLtQnRc0JkwqHab0VUVP7T3uBPSeYzxzDnpxPpUnHk=",
@@ -433,7 +433,7 @@
       "url": "_framework\/icudt_no_CJK.dat"
     },
     {
-      "hash": "sha256-NaYytu0N17woBJsPqV7lL0BNnscgjY5NC6mV9pffagc=",
+      "hash": "sha256-nHB5lrogHPac7BcMb66OQfXUxkrbaA464GnWCgvdfRE=",
       "url": "_framework\/blazor.boot.json"
     },
     {
@@ -441,11 +441,11 @@
       "url": "_framework\/dotnet.native.8.0.8.d2pmz982ox.js"
     },
     {
-      "hash": "sha256-j7a\/OH8w8h7TYHpRDwT0s58OZ3cKGujhaWrGeQTiGqo=",
+      "hash": "sha256-i9MzCLLKYAf2NPhjK6571pDocWKxOmyZF5Qh6dnRLHI=",
       "url": "_framework\/dotnet.native.wasm"
     },
     {
-      "hash": "sha256-guZ8J3DLRoMez\/5XwDetgdAnkQWouukpgRQxhyZb7HI=",
+      "hash": "sha256-UrlMGgiLAiENM+yMuguzZ9EHm+zddLO7OJhTA1PYsrA=",
       "url": "_framework\/AIApiWrapper.wasm"
     },
     {
@@ -713,5 +713,5 @@
       "url": "_content\/Habits.UIToolkit\/Habits.UIToolkit.bundle.scp.css"
     }
   ],
-  "version": "XkdbxXOT"
+  "version": "vKIFcqtt"
 };
